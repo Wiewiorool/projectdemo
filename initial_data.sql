6c55113922576e0b1b4898bcc9ddb9c84548ad7f
Wiewior <wasisnkipatryk@gmail.com>
2025-04-09 15:35:12 +0200

    Fix code.

--- initial_data.sql
+++ initial_data.sql
@@ -12,12 +12,14 @@ color varchar(10) not null,
 production_year date not null,
 horse_power varchar(10) not null,
 number_of_doors integer not null,
+type_of_car_id integer not null,
 FOREIGN KEY (type_of_car_id) references Type_of_car(type_of_car_id) on delete cascade
 );
 
 create table Car
 (
 car_id serial not null primary key,
+car_details_id varchar(20)not null,
 FOREIGN KEY (car_details_id) references Car_details(car_details_id) on delete cascade,
 buy_car_price decimal(8,2)not null
 );
@@ -30,6 +32,7 @@ surname varchar(10) not null
 
 create table Client
 (client_id serial not null primary key,
+personal_data_id integer not null,
 FOREIGN KEY (personal_data_id) references Personal_data(personal_data_id) on delete cascade,
 owned_cars integer not null,
 FOREIGN KEY (car_id) references Car(car_id) on delete cascade
@@ -38,13 +41,17 @@ FOREIGN KEY (car_id) references Car(car_id) on delete cascade
 create table Dealer
 (dealer_id serial not null primary key,
 degree varchar(10) not null,
+personal_data_id integer not null,
 FOREIGN KEY (personal_data_id) references Personal_data(personal_data_id) on delete cascade
 );
 
 create table Sale
 (sale_id serial not null primary key,
+dealer_id integer not null,
 FOREIGN KEY (dealer_id) references Dealer(dealer_id) on delete cascade,
+client_id integer not null,
 FOREIGN KEY (client_id) references Client(client_id) on delete cascade,
+car_id integer not null,
 FOREIGN KEY (car_id) references Car(car_id) on delete cascade ,
 date timestamp not null,
 sell_car_price decimal(8,2) not null
