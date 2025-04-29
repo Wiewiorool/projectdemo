e649be22593de804027c00e5130d81b71f89cb1d
Wiewior <wasisnkipatryk@gmail.com>
2025-04-29 17:22:00 +0200

    Add new class

---
+++ src/main/java/pl/wasinskipatryk/database/service/SalesService.java
@@ -0,0 +1,9 @@
+package pl.wasinskipatryk.database.service;
+
+import pl.wasinskipatryk.database.repositories.SaleRepository;
+
+public class SalesService {
+    private SaleRepository saleRepository;
+
+
+}
