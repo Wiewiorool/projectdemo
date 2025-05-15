b9eaf2fbad0f214b8dc60039c5e3ddb9eea1ce69
PaniOdProgramowania <pani.od.programowania@gmail.com>
2025-05-15 18:38:15 +0200

    [Lesson 15.05.2025] Start codeing, move method up

--- src/main/java/pl/wasinskipatryk/database/service/SalesService.java
+++ src/main/java/pl/wasinskipatryk/database/service/SalesService.java
@@ -1,5 +1,7 @@
 package pl.wasinskipatryk.database.service;
 
+import java.util.ArrayList;
+import java.util.Optional;
 import org.springframework.stereotype.Service;
 import pl.wasinskipatryk.database.enitities.*;
 import pl.wasinskipatryk.database.repositories.CarRepository;
@@ -33,11 +35,33 @@ public class SalesService {
      * @param clientName    - first name of a new client. This client has never been a client in our saloon so far.
      * @param clientSurname - surname of a new client. This client has never been a client in our saloon so far.
      * @param carId         - id of the sold car. This car should already exist in our IT system.
-     * @param commision     - commission added to the car's buyPrice.
+     * @param commission     - commission added to the car's buyPrice.
      *                      Example: if the saloon bought the car for 1000 PLN and commission is 1.8,
      *                      then it means the car was sold in the sale for 1000 * 1.8 = 1800 PLN.
-     * @return id of the newly crested sale.
+     * @return id of the newly created sale.
      */
+    public long registerNewSale(long dealerId, String clientName, String clientSurname, long carId, double commission) {
+
+        Optional<DealerEntity> dealerEntity = dealerRepository.findById(dealerId);
+        if (dealerEntity.isEmpty()) {
+            System.out.println("dealer " + dealerId + " was supposed to be int he system but can't find him");
+            throw new IllegalArgumentException("Dealer not found");
+        }
+        CarEntity carEntity = carRepository.findById(carId).get();
+        ClientEntity clientEntity = addNewClient(clientName, clientSurname);
+
+        SaleEntity newSale = SaleEntity.builder()
+            .car(carEntity)
+            .dealer(dealerEntity)
+            .date(Instant.now())
+            .clientId(clientEntity)
+            .sellCarPrice(carEntity.getBuyCarPrice().multiply(BigDecimal.valueOf(commission)))
+            .build();
+        saleRepository.save(newSale);
+
+
+        return newSale.getSaleId();
+    }
 
     public ClientEntity addNewClient(String clientName, String clientSurname) {
         ClientEntity newClient = ClientEntity.builder()
@@ -50,23 +74,6 @@ public class SalesService {
     }
 
 
-    public long registerNewSale(long dealerId, String clientName, String clientSurname, long carId, double commission) {
 
-        DealerEntity dealerEntity = dealerRepository.findById(dealerId).get();
-        CarEntity carEntity = carRepository.findById(carId).get();
-        ClientEntity clientEntity = addNewClient(clientName, clientSurname);
-
-        SaleEntity newSale = SaleEntity.builder()
-                .car(carEntity)
-                .dealer(dealerEntity)
-                .date(Instant.now())
-                .clientId(clientEntity)
-                .sellCarPrice(carEntity.getBuyCarPrice().multiply(BigDecimal.valueOf(commission)))
-                .build();
-        saleRepository.save(newSale);
-
-
-        return newSale.getSaleId();
-    }
 
 }
