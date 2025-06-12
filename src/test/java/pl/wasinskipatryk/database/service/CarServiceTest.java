18c41a9ebdf9b08805aa4efd1b4325cc7de9bcf8
Wiewior <wasisnkipatryk@gmail.com>
2025-06-12 19:30:14 +0200

    Clean code, rename column name because of forbidden name

--- src/test/java/pl/wasinskipatryk/database/service/CarServiceTest.java
+++ src/test/java/pl/wasinskipatryk/database/service/CarServiceTest.java
@@ -1,16 +1,21 @@
 package pl.wasinskipatryk.database.service;
 
-import java.math.BigDecimal;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ActiveProfiles;
+import pl.wasinskipatryk.database.enitities.CarEntity;
 import pl.wasinskipatryk.database.repositories.CarRepository;
 import pl.wasinskipatryk.demo.car.TypeOfCar;
 
+import java.math.BigDecimal;
+import java.util.List;
+
 @ActiveProfiles("test")
 @SpringBootTest
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
 class CarServiceTest {
 
     @Autowired
@@ -25,11 +30,28 @@ class CarServiceTest {
 
         // when
         long newCarId = carService.addNewCar("Golf", "gray", 2025, 140, 5,
-                                      TypeOfCar.HATCHBACK, BigDecimal.valueOf(10_000)
+                TypeOfCar.HATCHBACK, BigDecimal.valueOf(10_000)
         );
 
         // then
         Assertions.assertNotNull(carRepository.findById(newCarId));
 
+    }
+
+    @Test
+    void shouldGetAllCars() {
+        //given
+        carService.addNewCar("Audi", "gray", 2025, 140, 5,
+                TypeOfCar.HATCHBACK, BigDecimal.valueOf(10_000));
+
+        carService.addNewCar("BMW", "blue", 2025, 140, 2,
+                TypeOfCar.HATCHBACK, BigDecimal.valueOf(15_000));
+
+        //when
+        List<CarEntity> actual = carService.findAllCars();
+
+        //then
+        Assertions.assertNotNull(actual);
+        Assertions.assertEquals(2, actual.size());
     }
 }
